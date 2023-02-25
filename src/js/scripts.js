6bb03c774cee0b8f7cce6b69a13b6213eabf3059
Natalia <znatashaa13@gmail.com>
2023-02-25 15:22:18 +0200

    form validate

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -62,3 +62,87 @@ document.getElementById('scrolTop').addEventListener('click', function () {
         behavior: 'smooth'
     });
 });
+
+//validate form
+
+
+function validateEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+}
+
+const form = document.querySelector('#form');
+const nameUser = document.querySelector('#name');
+const emailUser = document.querySelector('#email');
+const messageUser = document.querySelector('#message');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // зупиняємо відправку форми
+    const name = nameUser.value;
+    const email = emailUser.value;
+    const message = messageUser.value;
+
+    if (name === '' || email === '' || message === '') {
+        alert('Будь ласка, заповніть всі поля форми');
+    }
+        // // перевіряємо валідність емейлу
+        // const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (validateEmail(emailUser.value)) {
+        alert('Будь ласка, введіть дійсну електронну адресу');
+        event.preventDefault(); // зупиняємо відправку форми
+    }
+
+    if(name || email || message) {
+        alert('good')
+}
+});
+
+
+
+
+// let sendMessage = document.querySelector("#sendMessage"),
+//     placeInfo = document.querySelector(".placeInfo")
+    
+// sendMessage.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     validateForm()
+// } ) 
+       
+// function validateForm() {
+//     let inputs = document.forms["myForm"].getElementsByTagName("input");
+//     for (let i = 0; i < inputs.length; i++) {
+//         if (inputs[i].value == "") {
+//             placeInfo.classList.add("placeInfoOpacity")
+//             return false;
+//         } 
+//     }
+//     // sendForm();
+// }
+
+//send form
+// function sendForm () {
+//     const form = document.querySelector('#form'); 
+//     const nameInput = document.querySelector('#name'); 
+//     const emailInput = document.querySelector('#email'); 
+//     const messageInput = document.querySelector('#message'); 
+     
+//     form.addEventListener('submit', function (event) { 
+//       event.preventDefault(); // Зупиняємо стандартну поведінку форми 
+     
+//       const xhr = new XMLHttpRequest(); 
+//       xhr.onreadystatechange = function () { 
+//         if (xhr.readyState === 4) { 
+//           if (xhr.status === 200) { 
+//             alert('Повідомлення надіслано. Дякуємо!'); 
+//           } else { 
+//             alert('Виникла помилка під час відправки повідомлення.'); 
+//           } 
+//         } 
+//       }; 
+//       xhr.open('POST', 'index.php', true); 
+//       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
+//       let formData = `name=${nameInput.value}&email=${emailInput.value}&message=${messageInput.value}`; 
+//       xhr.send(formData); 
+//     });
+// }
+
